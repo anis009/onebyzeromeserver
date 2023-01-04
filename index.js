fa987de68abe6115f7df75c9559177dbdd0ac74f
anichu <anis.cse5.bu@gmail.com>
2023-01-05 00:20:22 +0600

    changed

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ app.post("/courses", async (req, res) => {
 });
 
 app.get("/", async (req, res) => {
-	res.send(`running server on  sin`);
+	res.send(`running server on  sina`);
 });
 
 app.post("/api/year", async (req, res) => {
