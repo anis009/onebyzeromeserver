5f13a7bb1dd06afeb88f9a41c642565ff9229c62
anichu <anis.cse5.bu@gmail.com>
2023-02-12 12:00:51 +0600

    change api

--- controllers/courseControllers.js
+++ controllers/courseControllers.js
@@ -1,6 +1,6 @@
 const Course = require("../models/courseModel");
 
 exports.getCourses = async (req, res) => {
-	const courses = await Course.find().sort({ year: 1, semester: 1 });
+	const courses = await Course.find().sort({ year: 1 });
 	res.send(courses);
 };
